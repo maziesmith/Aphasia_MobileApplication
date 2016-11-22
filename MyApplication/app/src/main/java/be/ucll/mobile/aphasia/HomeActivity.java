2e5faeddca240a2fa31c03c7681e2cf1abd52e16
tomploem <tom.ploem@gmail.com>
2016-11-22 21:37:05 +0100

    merge of all branches with creation of homepage layout

--- MyApplication/app/src/main/java/be/ucll/mobile/aphasia/HomeActivity.java
+++ MyApplication/app/src/main/java/be/ucll/mobile/aphasia/HomeActivity.java
@@ -1,7 +1,10 @@
 package be.ucll.mobile.aphasia;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TextView txt = (TextView) findViewById(R.id.slogan);
+        Button start = (Button) findViewById(R.id.start_test);
+        Button results = (Button) findViewById(R.id.results);
+        Button adjust = (Button) findViewById(R.id.adjust);
+
+        Typeface latoLightItalic = Typeface.createFromAsset(getAssets(), "fonts/Lato-LightItalic.ttf");
+        Typeface latoBold = Typeface.createFromAsset(getAssets(), "fonts/Lato-Bold.ttf");
+        txt.setTypeface(latoLightItalic);
+        start.setTypeface(latoBold);
+        results.setTypeface(latoBold);
+        adjust.setTypeface(latoBold);
+
     }
+
 }
