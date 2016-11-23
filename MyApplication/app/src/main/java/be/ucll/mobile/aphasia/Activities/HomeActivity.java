5c2b4dfbbab7873e99676fc730983675f2d1bf42
Tom Ploem <tom.ploem@gmail.com>
2016-11-23 01:05:10 +0100

    update: menu consistency, custom xml tag (TextView), gradient drawables and changed file extension to PNG of the photo library pictures

--- MyApplication/app/src/main/java/be/ucll/mobile/aphasia/Activities/HomeActivity.java
+++ MyApplication/app/src/main/java/be/ucll/mobile/aphasia/Activities/HomeActivity.java
@@ -1,10 +1,14 @@
-package be.ucll.mobile.aphasia;
+package be.ucll.mobile.aphasia.Activities;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import be.ucll.mobile.aphasia.R;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,6 +16,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setFonts();
+        setToolbarTitle();
+    }
+
+    private void setToolbarTitle() {
+        TextView textView = (TextView) findViewById(R.id.toolbar_title);
+        textView.setText("Home");
+    }
+
+    private void setFonts() {
 
         TextView txt = (TextView) findViewById(R.id.slogan);
         Button start = (Button) findViewById(R.id.start_test);
@@ -20,11 +34,29 @@ public class HomeActivity extends AppCompatActivity {
 
         Typeface latoLightItalic = Typeface.createFromAsset(getAssets(), "fonts/Lato-LightItalic.ttf");
         Typeface latoBold = Typeface.createFromAsset(getAssets(), "fonts/Lato-Bold.ttf");
+
         txt.setTypeface(latoLightItalic);
         start.setTypeface(latoBold);
         results.setTypeface(latoBold);
         adjust.setTypeface(latoBold);
-
     }
 
+    public void openResultsIntent(View view) {
+        Intent i = new Intent(this, ResultsActivity.class);
+        i.setAction(Intent.ACTION_VIEW );
+        startActivity(i);
+    }
+
+    public void openAdjustIntent(View view) {
+        Intent i = new Intent(this, AdjustActivity.class);
+        i.setAction(Intent.ACTION_VIEW );
+        startActivity(i);
+    }
+
+    public void openQuizIntent(View view) {
+        Intent i = new Intent(this, QuizActivity.class);
+       i.setAction(Intent.ACTION_VIEW );
+        startActivity(i);
+        System.out.println("WOW NO ERROR");
+    }
 }
---
+++ MyApplication/app/src/main/java/be/ucll/mobile/aphasia/Activities/QuizActivity.java
@@ -0,0 +1,27 @@
+package be.ucll.mobile.aphasia.Activities;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import be.ucll.mobile.aphasia.R;
+
+/**
+ * Created by tompl on 11/22/2016.
+ */
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        setToolbarTitle();
+    }
+
+    private void setToolbarTitle() {
+        TextView textView = (TextView) findViewById(R.id.toolbar_title);
+        textView.setText("Quiz");
+    }
+}
