582dcc3266af616e684bb5461fb4d8df37cad2d4
Sebastiaan Viaene <Sebastiaan Viaene>
2016-12-14 16:58:22 +0100

    Begin of QuizActivity

--- MyApplication/app/src/main/java/be/ucll/mobile/aphasia/Activities/QuizActivity.java
+++ MyApplication/app/src/main/java/be/ucll/mobile/aphasia/Activities/QuizActivity.java
@@ -1,10 +1,21 @@
 package be.ucll.mobile.aphasia.Activities;
 
 
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import be.ucll.mobile.aphasia.Model.CustomSwipeAdapter;
+import be.ucll.mobile.aphasia.Model.ImageItem;
 import be.ucll.mobile.aphasia.R;
 
 /**
@@ -12,12 +23,32 @@ import be.ucll.mobile.aphasia.R;
  */
 
 public class QuizActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    CustomSwipeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         setToolbarTitle();
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        adapter = new CustomSwipeAdapter(this);
+        viewPager.setAdapter(adapter);
+
+    }
+
+
+    private ArrayList<ImageItem> getData() throws IOException {
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+        Resources res = getResources(); //if you are in an activity
+        AssetManager am = res.getAssets();
+        String[] imgPath = am.list("images");
+        for (int j = 0; j< imgPath.length; j++) {
+            InputStream is = am.open("images/" + imgPath[j]);
+            Bitmap bitmap = BitmapFactory.decodeStream(is);
+            imageItems.add(new ImageItem(bitmap, imgPath[j]));
+        }
+        return imageItems;
     }
 
     private void setToolbarTitle() {
